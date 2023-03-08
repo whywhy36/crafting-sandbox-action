4da70bd9caff7f404f405776a097388fcbe6f372
Yuan Zhuang <whywhy36@gmail.com>
2023-03-09 08:31:03 +1300

    add extra logging

--- src/parser.ts
+++ src/parser.ts
@@ -6,6 +6,7 @@ export function parseParams(): SandboxParams {
   const name = core.getInput('name')
   const paramsFile = core.getInput('launch')
   const jsonString = fs.readFileSync(paramsFile).toString()
+  core.debug(`jsonString: ${jsonString}`)
   jsonString.replace('$BRANCH', process.env.GITHUB_HEAD_REF || '')
   return {
     ...JSON.parse(jsonString),

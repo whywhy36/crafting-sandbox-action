323ea70fe96ec5770bd8aed37e8eb218ddc2d71b
Yuan Zhuang <whywhy36@gmail.com>
2023-03-09 09:44:15 +1300

    update dist

--- dist/index.js
+++ dist/index.js
@@ -131,12 +131,20 @@ exports.parseParams = void 0;
 const core = __importStar(__nccwpck_require__(186));
 const fs = __importStar(__nccwpck_require__(747));
 function parseParams() {
+    const baseSandboxParams = {
+        template: '',
+        sandboxName: '',
+        workspaces: [],
+        dependencies: [],
+        containers: [],
+        autoLaunch: false
+    };
     const name = core.getInput('name');
     const paramsFile = core.getInput('launch');
     const jsonString = fs.readFileSync(paramsFile).toString();
     core.debug(`jsonString: ${jsonString}`);
     jsonString.replace('$BRANCH', process.env.GITHUB_HEAD_REF || '');
-    return Object.assign(Object.assign({}, JSON.parse(jsonString)), { sandboxName: name });
+    return Object.assign(Object.assign(Object.assign({}, baseSandboxParams), JSON.parse(jsonString)), { sandboxName: name });
 }
 exports.parseParams = parseParams;
 

ea8cb8a6b7030d972a75d1cee70dcbaccf254112
Yuan Zhuang <whywhy36@gmail.com>
2023-03-09 09:43:37 +1300

    introduce base sandbox params

--- src/parser.ts
+++ src/parser.ts
@@ -3,12 +3,21 @@ import * as fs from 'fs'
 import {SandboxParams} from './generator'
 
 export function parseParams(): SandboxParams {
+  const baseSandboxParams: SandboxParams = {
+    template: '',
+    sandboxName: '',
+    workspaces: [],
+    dependencies: [],
+    containers: [],
+    autoLaunch: false
+  }
   const name = core.getInput('name')
   const paramsFile = core.getInput('launch')
   const jsonString = fs.readFileSync(paramsFile).toString()
   core.debug(`jsonString: ${jsonString}`)
   jsonString.replace('$BRANCH', process.env.GITHUB_HEAD_REF || '')
   return {
+    ...baseSandboxParams,
     ...JSON.parse(jsonString),
     sandboxName: name
   } as SandboxParams

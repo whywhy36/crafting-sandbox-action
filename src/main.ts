77e89ce5fcc6f43ce0070330f1fc17be4b46558a
Yuan Zhuang <whywhy36@gmail.com>
2023-03-09 10:33:25 +1300

    update

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 import {generateSandboxLaunchURL} from './generator'
 import {parseParams} from './parser'
 
@@ -7,6 +8,23 @@ async function run(): Promise<void> {
     const sandboxParams = parseParams()
     const url = await generateSandboxLaunchURL(sandboxParams)
     core.setOutput('url', url)
+    const octokit = github.getOctokit(process.env.GITHUB_TOKEN || '')
+    const {owner, repo} = github.context.repo
+    const number = github.context.payload.pull_request?.number
+    if (number) {
+      await octokit.request(
+        'POST /repos/{owner}/{repo}/issues/{pull_number}/comments',
+        {
+          owner,
+          repo,
+          issue_number: number,
+          body: `Crafting Sandbox (Preview)[${url}]`,
+          headers: {
+            'X-GitHub-Api-Version': '2022-11-28'
+          }
+        }
+      )
+    }
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
